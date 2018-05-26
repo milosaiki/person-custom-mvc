bca116244e59d686bca55ecee8e1e746fe2d60b7
misko <milosaiki@gmail.com>
2018-05-26 17:45:43 +0200

    'dev'

--- src/Classes/ManageFile.php
+++ src/Classes/ManageFile.php
@@ -15,7 +15,8 @@ class ManageFile
   public function createFile()
   {
     /** make directory if not exist */
-    if (!file_exists($this->filename)) {
+    if (!file_exists( $this->filename) ) 
+    {
       $file = fopen($this->filename, "w") or die("Unable to open file!");
     } else {
       $file = fopen($this->filename, "a") or die("Unable to open file!");
